63db1b5eef29518138096a7e83568a365c91ab88
david <chengjie.ding@gmail.com>
2016-01-07 14:26:34 +0800

    add flash message and receive from information

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # -*- coding=utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
 from .forms import LoginForm
@@ -10,6 +10,9 @@ from .forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
