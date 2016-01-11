9cf92b9bfca30686e79c69a0bb2d521d8e706aa1
david <chengjie.ding@gmail.com>
2016-01-11 17:08:36 +0800

    add user profile page

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 from app import db
+from hashlib import md5
 
 
 class User(db.Model):
@@ -22,6 +23,9 @@ class User(db.Model):
             return unicode(self.id)  # python 2
         except NameError:
             return str(self.id)  # python 3
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
     def __repr__(self):
         return '<User %r>' % self.nickname
--- app/views.py
+++ app/views.py
@@ -78,3 +78,19 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
