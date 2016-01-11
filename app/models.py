5b8d3ddf085734a4e6ea3f1cbf90886224a7b945
david <chengjie.ding@gmail.com>
2016-01-11 13:29:38 +0800

    login & logout using Flask-Login and Flask-OpenID

--- app/models.py
+++ app/models.py
@@ -4,9 +4,24 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    nickname = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
 
     def __repr__(self):
         return '<User %r>' % self.nickname
